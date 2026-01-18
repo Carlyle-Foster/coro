acbe95779d3f39ac18a1a07a135fa42433eb72b5
Carlyle <hack366@pm.me>
2026-01-18 12:11:27 -0800

    no longer save unecessary registers

--- impl_amd64_posix.asm
+++ impl_amd64_posix.asm
@@ -16,8 +16,6 @@ global swap_stacks
 %define finished   0
 
 %macro prelude 0
-    push rdi
-    push rsi
     push rdx
     push rbp
     push rbx
@@ -25,17 +23,6 @@ global swap_stacks
     push r13
     push r14
     push r15
-    sub rsp, 10*16
-    movaps rsp[9*16], xmm6
-    movaps rsp[8*16], xmm7
-    movaps rsp[7*16], xmm8
-    movaps rsp[6*16], xmm9
-    movaps rsp[5*16], xmm10
-    movaps rsp[4*16], xmm11
-    movaps rsp[3*16], xmm12
-    movaps rsp[2*16], xmm13
-    movaps rsp[1*16], xmm14
-    movaps rsp[0*16], xmm15
 
     mov rax, [rdi]
     mov [rdi], rsp
@@ -44,17 +31,6 @@ global swap_stacks
 %endmacro
 
 %macro postlude 1
-    movaps xmm15, rsp[0*16]
-    movaps xmm14, rsp[1*16]
-    movaps xmm13, rsp[2*16]
-    movaps xmm12, rsp[3*16]
-    movaps xmm11, rsp[4*16]
-    movaps xmm10, rsp[5*16]
-    movaps xmm9,  rsp[6*16]
-    movaps xmm8,  rsp[7*16]
-    movaps xmm7,  rsp[8*16]
-    movaps xmm6,  rsp[9*16]
-    add rsp, 10*16
     pop r15
     pop r14
     pop r13
@@ -62,8 +38,6 @@ global swap_stacks
     pop rbx
     pop rbp
     pop rdx
-    pop rsi
-    pop rdi
 
     mov rax, %1
     ret
